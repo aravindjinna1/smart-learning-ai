953f7baf899be0f81f93a8a905d49c3f504a8790
Aravind jinna <aravindjinna1@gmail.com>
2026-02-26 23:44:58 +0530

    CORS Resolved

--- server/server.js
+++ server/server.js
@@ -1,33 +1,33 @@
-
-const express = require('express');
+const express = require("express");
 const server = express();
-const DbConnection = require('./Database/DbConnection')
-const appRoutes = require('./app')
+const DbConnection = require("./Database/DbConnection");
+const appRoutes = require("./app");
 
-require('dotenv').config();
+require("dotenv").config();
 
 DbConnection();
 server.use(express.json());
 
-
 const cors = require("cors");
 
-server.use(cors({
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST"],
-}));
+server.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://smart-learning-ai-green.vercel.app/Ask-ai",
+    ],
+    methods: ["GET", "POST"],
+  }),
+);
 
+server.get("/", (req, res) => {
+  res.send("server running");
+});
 
-server.get('/', (req, res)=>{
-    res.send("server running")
-})
+server.use("/api", appRoutes);
 
-server.use('/api', appRoutes)
+const PORT = process.env.PORT || 5000;
 
-
-
-const PORT = process.env.PORT || 5000 
-
-server.listen(PORT, ()=>{
-    console.log("server listening at http://localhost:5000")
-})
+server.listen(PORT, () => {
+  console.log("server listening at http://localhost:5000");
+});
